54002a6ce6b0df5cf8c5a8fe3b0f43857876f853
Tec. Guardia Sebastian Nicolas <105818380+sguardia25@users.noreply.github.com>
2024-04-15 01:55:03 -0300

    Create mysqli_crearbase.php

---
+++ BASE DE DATOS/PHP/MYSQLI/mysqli_crearbase.php
@@ -0,0 +1,24 @@
+<?php
+
+    //CONEXION MYSQLI
+    $servidor="localhost";
+    $usuario="root";
+    $password="";
+
+    $conexion=new mysqli($servidor,$usuario,$password);
+
+    if($conexion->connect_error){
+        die("ERRO DE CONEXION".$conexion->connect_error);
+    }
+    echo"CONEXION EXITOSA";
+
+    //CREAR BASE DE DATOS
+    $sql="CREATE DATABASE Practicas";
+
+    if ($conexion->query($sql)===TRUE) {
+        echo "BASE DE DATOS CREADA";
+    }else {
+        echo "ERROR".$conexion->error;
+    }
+    
+?>
