a9d50c635f5e81b0d620da147adf049a3daff629
Tec. Guardia Sebastian Nicolas <105818380+sguardia25@users.noreply.github.com>
2024-04-15 01:38:32 -0300

    Update index.php

--- HTML/index.php
+++ HTML/index.php
@@ -1,1 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head> <!--ESTRUCTURA HEAD-->
+    <title>
+        PRACTICA HTML 
+    </title> <!--TITULO PIE DE PAGINA-->
+</head>
+<body> <!--ESTRUCTURA BODY-->
+    <h1>
+        PRACTICAS PROFESIONALIZANTES <!-- TITULOS-->
+    </h1>
+    <h2>
+        PRACTICAS PROFESIONALIZANTES
+    </h2>
+    <h3>
+        PRACTICAS PROFESIONALIZANTES
+    </h3>
+    <h4>
+        PRACTICAS PROFESIONALIZANTES
+    </h4>
+    <p id="parrafo-id"><!--PARRAFOS-->
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quo deleniti assumenda,
+        doloribus, soluta consequatur, a ipsa dolor repudiandae omnis veritatis consectetur!
+        Voluptas velit, eligendi praesentium quibusdam facere ab laboriosam.
+    </p>
+    <br> <!--SALTO DE PAGINA-->
+    <p class="parrafo">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quo deleniti assumenda,
+        doloribus, soluta consequatur, a ipsa dolor repudiandae omnis veritatis consectetur!
+        Voluptas velit, eligendi praesentium quibusdam facere ab laboriosam.   
+    </p>
+    <hr> <!--LINEA-->
+    <p class="parrafo">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quo deleniti assumenda,
+        doloribus, soluta consequatur, a ipsa dolor repudiandae omnis veritatis consectetur!
+        Voluptas velit, eligendi praesentium quibusdam facere ab laboriosam.
+    </p>
+    <a href="https://www.google.com/" target="_black">LINK</a> <!--LINK-->
 
+    <img src="media/DEKU.png" alt="DEKU"> <!--IMAGEN-->
+
+    <link rel="stylesheet" href="stylos.css"> <!--STYLE-->
+
+    <div>
+        <h1>TITULO DIV</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quo deleniti assumenda,
+            doloribus, soluta consequatur, a ipsa dolor repudiandae omnis veritatis consectetur!
+            Voluptas velit, eligendi praesentium quibusdam facere ab laboriosam.
+        </p>
+    </div>
+    <main>
+
+    </main>
+    <section id="info">
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quo deleniti assumenda,
+            doloribus, soluta consequatur, a ipsa dolor repudiandae omnis veritatis consectetur!
+            Voluptas velit, eligendi praesentium quibusdam facere ab laboriosam.
+        </div>
+
+    </section>
+    <footer>
+
+    </footer>
+    <header>
+
+    </header>
+</body>
+
+</html>
