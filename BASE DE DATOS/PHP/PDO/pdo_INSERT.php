904c633eb7af1aa3529642a6dfd490d6f1001b46
Tec. Guardia Sebastian Nicolas <105818380+sguardia25@users.noreply.github.com>
2024-04-15 02:06:49 -0300

    Create pdo_INSERT.php

---
+++ BASE DE DATOS/PHP/PDO/pdo_INSERT.php
@@ -0,0 +1,24 @@
+<?php
+
+    //CONEXION PDO
+    $servidor="localhost";
+    $usuario="root";
+    $password="";
+    $basededatos="PracticasPDO";
+
+    try {
+        $conexion= new PDO("mysql:host=$servidor; dbname=$basededatos",$usuario,$password);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "CONEXION EXITOSA";
+
+         //INSERT
+         $sql="INSERT INTO alumnos(id, nombre, apellido) VALUES (NULL, 'SEBASTIAN', 'GUARDIA')";
+
+         $conexion->exec($sql);
+         echo"ALUMNO INGRESADO";
+         
+    }catch(PDOException $error){
+        echo "ERROR".$error->getMessage();
+    }
+
+?>
