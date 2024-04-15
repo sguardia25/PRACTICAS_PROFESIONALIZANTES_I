fb6d2fc04d96406c48ada6154239a0bfa6f16c7c
Tec. Guardia Sebastian Nicolas <105818380+sguardia25@users.noreply.github.com>
2024-04-15 01:51:49 -0300

    Create php_html.php

---
+++ PHP/php_html.php
@@ -0,0 +1,19 @@
+<?php
+    //CABECERA
+    header('Content-Type: text/html; charset=UFT-8');
+    
+    //VARIABLE CHAR
+    $respuesta='';
+
+    $respuesta= '<!DOCTYPE html>';
+    $respuesta= '<html lang="es">';
+    $respuesta= ''; //ESPACIOS
+    $respuesta= '<head>';
+    $respuesta= '<meta charset= "UFT-8">';
+    $respuesta= '<meta name="vievport" contect="width=device-width, initial-scalet=1.0"';
+    $respuesta= '<title> PHP/HTML </title>';
+    $respuesta= '</head>';
+    $respuesta= '';
+    $respuesta= '<body>';
+    $respuesta= ''; 
+?>
