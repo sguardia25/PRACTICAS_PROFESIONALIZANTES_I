030ba9aaac0b46d2655a9cbfb16af53abf6d4419
Tec. Guardia Sebastian Nicolas <105818380+sguardia25@users.noreply.github.com>
2024-04-15 01:45:48 -0300

    Create variables.php

---
+++ PHP/variables.php
@@ -0,0 +1,29 @@
+<?php
+    //VARIABLE NUMERICA
+    $numero= 5;
+    echo "Esto es una variable numerica= $numero";
+
+    //VARIABLE TEXTO
+    $palabra= 'palabra';
+    echo "Esto es una variable texto= $palabra";
+
+    //VARIABLE BOOLEANA
+    $booleana= true;
+    echo "Esto es una variable booleana= $booleana";
+
+    //VARIABLE ARREGLO
+    $arreglo= array("rojo","amarrillo", "azul");
+    echo "Esto es una variable array= $arreglo[0]";
+
+    //VARIABLE ARREGLO CON PROPIEDADES
+    $arreglo2= array ("propiedad1"=>"rojo", "propiedad2"=>"amarillo", "propiedad3"=>"azul");
+    echo "Esto es una variable array-propiedad = $arreglo2[propiedad1]";
+    
+    //VARIABLE OBJETO
+    $objeto= (object)["objeto1"=>"rojo", "objeto2"=>"amarillo", "objeto3"=>"azul"];
+    echo "Esto es una variable objeto = $objeto->objeto1";
+ 
+    //VARIABLE VARDUMP
+    $numero= 10;
+    var_dump($numero);
+?>
